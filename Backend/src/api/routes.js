4d52b027845234a6b598978e88b9ed0b18f31f36
lvhills <loveswigo@gmail.com>
2025-05-10 16:44:37 +0700

    admin login

--- Backend/src/api/routes.js
+++ Backend/src/api/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const menu = require('./menu/route');
+const admin = require('./admin/route')
 module.exports = () => {
   const app = express.Router();
 
   menu(app);
+  app.use('/admin', admin)
 
   return app;
 };
