e94348c7c43411a5d444b31009e4ccc31bca71c7
lvhills <loveswigo@gmail.com>
2025-05-14 02:21:08 +0700

    change pass fitur

--- Backend/src/api/admin/repository.js
+++ Backend/src/api/admin/repository.js
@@ -1,9 +1,16 @@
 const db = require('../../core/db');
+const bcrypt = require('bcrypt');
 
 const getAdminByEmail = async (email) => {
-  return db('admin').where({ email }).first();
+  return await db('admin').where({ email }).first();
+};
+
+const updatePassword = async (email, newPassword) => {
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  await db('admin').where({ email }).update({ password: hashedPassword });
 };
 
 module.exports = {
   getAdminByEmail,
+  updatePassword
 };
