b4f45505e095c46bf296fadffacd309f08175d56
fortunecookiees <162522300+fortunecookiees@users.noreply.github.com>
2025-05-30 16:16:59 +0700

    ViewOrder

--- Backend/src/api/payment/controller.js
+++ Backend/src/api/payment/controller.js
@@ -1,163 +1,162 @@
 
-const { createTransaction, createTransactionItems, updatePaymentStatus, cekPaymentId, updateOrderPayment, getPackagePriceById, getPeriodPriceById } = require('./repository.js');
-const { MIDTRANS_APP_URL, MIDTRANS_SERVER_KEY, PENDING_PAYMENT } = require('../../utils/constant.js');
-const { errorResponder, errorTypes } = require('../../core/errors');
-const fetch = require('node-fetch'); 
+  const { createTransaction, createTransactionItems, updatePaymentStatus, cekPaymentId, updateOrderPayment, getPackagePriceById, getPeriodPriceById } = require('./repository.js');
+  const { MIDTRANS_APP_URL, MIDTRANS_SERVER_KEY, PENDING_PAYMENT } = require('../../utils/constant.js');
+  const { errorResponder, errorTypes } = require('../../core/errors');
+  const fetch = require('node-fetch'); 
 
-const getNanoid = async () => {
-  const { nanoid } = await import('nanoid');
-  return nanoid;
-};
+  const getNanoid = async () => {
+    const { nanoid } = await import('nanoid');
+    return nanoid;
+  };
 
 
-const createTransactionCtrl = async (req, res) => {
-     console.log("Incoming transaction payload:", req.body);
-    const { customerId, orderId, packageId, periodId } = req.body;
+  const createTransactionCtrl = async (req, res) => {
+      console.log("Incoming transaction payload:", req.body);
+      const { customerId, orderId, packageId, periodId } = req.body;
+
+      try {
+          const nanoid = await getNanoid();
+          const transactionId = `TRX-${nanoid(4)}-${nanoid(8)}`;
+
+          const packageResult = await getPackagePriceById(packageId);
+          const periodResult = await getPeriodPriceById(periodId);
+
+          const packagePrice = packageResult?.[0]?.package_price;
+          const periodPrice = periodResult?.[0]?.period_price;
+
+          if (!packagePrice || !periodPrice) {
+              return res.status(404).json(errorResponder(errorTypes.NOT_FOUND));
+          }
+          console.log('Package ID:', packageId, 'Price:', packagePrice);
+          console.log('Period ID:', periodId, 'Price:', periodPrice);
+          const gross_amount = Number(packagePrice) * Number(periodPrice);
+          console.log('Gross amount:', gross_amount);
+          const authString = Buffer.from(`${MIDTRANS_SERVER_KEY}:`).toString('base64');
+
+          //data payload midtrans
+          const payload = {
+              transaction_details: {
+                  order_id: transactionId,
+                  gross_amount
+              },
+              item_details: [
+              {
+                  id: `${packageId}-${periodId}`,
+                  price: gross_amount,
+                  quantity: 1,
+                  name: `Custom Package #${packageId} (Period ${periodPrice} weeks)`
+              }
+              ],
+              customer_details: {
+                  customerId,
+                  orderId
+              },
+              callbacks: {
+                  finish: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
+                  cancel: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
+                  pending: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
+              }
+          };
+          
+          const response = await fetch(`${MIDTRANS_APP_URL}/snap/v1/transactions`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json',
+                  'Authorization': `Basic ${authString}`
+              },
+              body: JSON.stringify(payload)
+          });
+
+          const data = await response.json();
+
+          if (response.status !== 201) {
+              console.log("error not ok")
+              return res.status(500).json(errorResponder(errorTypes.INTERNAL_SERVER));
+          }
+          
+          await Promise.all([
+              createTransaction({
+                  transactionId,
+                  gross_amount,
+                  customerId,
+                  orderId,
+                  snap_token: data.token,
+                  snap_redirect_url: data.redirect_url
+              }),
+              createTransactionItems({
+                  transactionId,
+                  packageId,
+                  periodId
+              })
+          ]);
+
+          res.json({
+              status: 'success',
+              data: {
+                  transactionId,
+                  status: PENDING_PAYMENT,
+                  customerId,
+                  orderId,
+                  packageId,
+                  periodId,
+                  gross_amount,
+                  snap_token: data.token,
+                  snap_redirect_url: data.redirect_url,
+              }
+          });
+      } catch (error) {
+          console.error(error);
+          return res.status(500).json(errorResponder(errorTypes.INTERNAL_SERVER));
+      }
+  };
+
+  const updatePaymentCtrl = async (req, res) => {
+    try {
+      const { transactionId } = req.params;
+
+      // Update payment status di table transactions
+      const updated = await updatePaymentStatus(transactionId);
+      if (!updated) {
+        return res.status(404).json({ message: 'Transaction not found' });
+      }
+
+      // Ambil data transaksi untuk dapat orderId
+      const trx = await cekPaymentId(transactionId);
+      if (!trx) {
+        return res.status(404).json({ message: 'Transaction data not found' });
+      }
+
+      await updateOrderPayment(trx.orderId, trx.transactionId); 
+
+      return res.status(200).json({ message: 'Payment status updated successfully' });
+    } catch (error) {
+      console.error('Error updating payment status:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+
+  const handleCallback = async (req, res) => {
+    const { order_id, transaction_status } = req.body;
 
     try {
-        const nanoid = await getNanoid();
-        const transactionId = `TRX-${nanoid(4)}-${nanoid(8)}`;
+      // Cek jika pembayaran sukses
+      if (transaction_status === 'settlement' || transaction_status === 'capture') {
+        await updatePaymentStatus(order_id); 
 
-        const packageResult = await getPackagePriceById(packageId);
-        const periodResult = await getPeriodPriceById(periodId);
+        return res.status(200).json({ message: 'Payment success & updated.' });
+      }
 
-        const packagePrice = packageResult?.[0]?.package_price;
-        const periodPrice = periodResult?.[0]?.period_price;
-
-        if (!packagePrice || !periodPrice) {
-            return res.status(404).json(errorResponder(errorTypes.NOT_FOUND));
-        }
-        console.log('Package ID:', packageId, 'Price:', packagePrice);
-        console.log('Period ID:', periodId, 'Price:', periodPrice);
-        const gross_amount = Number(packagePrice) * Number(periodPrice);
-        console.log('Gross amount:', gross_amount);
-        const authString = Buffer.from(`${MIDTRANS_SERVER_KEY}:`).toString('base64');
-
-        //data payload midtrans
-        const payload = {
-            transaction_details: {
-                order_id: transactionId,
-                gross_amount
-            },
-            item_details: [
-            {
-                id: `${packageId}-${periodId}`,
-                price: gross_amount,
-                quantity: 1,
-                name: `Custom Package #${packageId} (Period ${periodPrice} weeks)`
-            }
-            ],
-            customer_details: {
-                customerId,
-                orderId
-            },
-            callbacks: {
-                finish: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
-                cancel: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
-                pending: `${process.env.FRONT_END_URL}/order-status?transaction_id=${transactionId}`,
-            }
-        };
-        
-        const response = await fetch(`${MIDTRANS_APP_URL}/snap/v1/transactions`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Basic ${authString}`
-            },
-            body: JSON.stringify(payload)
-        });
-
-        const data = await response.json();
-
-        if (response.status !== 201) {
-            console.log("error not ok")
-            return res.status(500).json(errorResponder(errorTypes.INTERNAL_SERVER));
-        }
-        
-        await Promise.all([
-            createTransaction({
-                transactionId,
-                gross_amount,
-                customerId,
-                orderId,
-                snap_token: data.token,
-                snap_redirect_url: data.redirect_url
-            }),
-            createTransactionItems({
-                transactionId,
-                packageId,
-                periodId
-            })
-        ]);
-
-        res.json({
-            status: 'success',
-            data: {
-                transactionId,
-                status: PENDING_PAYMENT,
-                customerId,
-                orderId,
-                packageId,
-                periodId,
-                gross_amount,
-                snap_token: data.token,
-                snap_redirect_url: data.redirect_url,
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json(errorResponder(errorTypes.INTERNAL_SERVER));
+      res.status(200).json({ message: 'Payment not completed yet.' });
+    } catch (err) {
+      console.error('Error in Midtrans webhook:', err);
+      res.status(500).json({ error: 'Webhook processing failed.' });
     }
-};
-
-const updatePaymentCtrl = async (req, res) => {
-  try {
-    const { transactionId } = req.params;
-
-    // Update payment status di table transactions
-    const updated = await updatePaymentStatus(transactionId);
-    if (!updated) {
-      return res.status(404).json({ message: 'Transaction not found' });
-    }
-
-    // Ambil data transaksi untuk dapat orderId
-    const trx = await cekPaymentId(transactionId);
-    if (!trx) {
-      return res.status(404).json({ message: 'Transaction data not found' });
-    }
-
-    await updateOrderPayment(trx.orderId, trx.transactionId); 
-
-    return res.status(200).json({ message: 'Payment status updated successfully' });
-  } catch (error) {
-    console.error('Error updating payment status:', error);
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-const handleCallback = async (req, res) => {
-  const { order_id, transaction_status } = req.body;
-
-  try {
-    // Cek jika pembayaran sukses
-    if (transaction_status === 'settlement' || transaction_status === 'capture') {
-      await updatePaymentStatus(order_id); 
-
-      return res.status(200).json({ message: 'Payment success & updated.' });
-    }
-
-    res.status(200).json({ message: 'Payment not completed yet.' });
-  } catch (err) {
-    console.error('Error in Midtrans webhook:', err);
-    res.status(500).json({ error: 'Webhook processing failed.' });
-  }
-};
+  };
 
 
-
-module.exports = {
-    createTransactionCtrl,
-    updatePaymentCtrl,
-    handleCallback
-};
+  module.exports = {
+      createTransactionCtrl,
+      updatePaymentCtrl,
+      handleCallback,
+  };
--- Backend/src/api/payment/route.js
+++ Backend/src/api/payment/route.js
@@ -6,7 +6,7 @@ const upload = require('../../core/multer_config');
 
 module.exports = (app) => {
     app.use(router);
-
+    
     router.post('/transaction', createTransactionCtrl);
     router.put('/paymentSuccess/:transactionId', updatePaymentCtrl);
     router.post('/midtrans/callback', handleCallback);
